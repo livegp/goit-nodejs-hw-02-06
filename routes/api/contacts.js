a4c337fa81f498b5f64b0bce8d4a5c9bc2ad1629
Oleksandr <live.gp@gmail.com>
2023-09-24 12:37:01 +0300

    fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,67 @@
-import { Router } from 'express'
+import { Router } from "express";
+import contacts from "../../models/contacts.js";
+import httpError from "../../helpers/httpError.js";
+import Joi from "joi";
 
-const router = Router()
+const router = Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const addSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().min(10).max(15).required(),
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {{}
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw httpError(404, "Contact not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }    
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw httpError(400, error.message);
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  } 
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    // const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  } 
+});
 
-export default router
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    // const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  } 
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-import { listen } from './app.js'
+import app from './app.js';
 
-listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running. Use our API on port: ${port}`);
+});
