08b7d4384d3acc9d10e8fc8eb78cf26f9d0363df
Oleksandr <live.gp@gmail.com>
2023-09-27 23:13:47 +0300

    fix message

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,25 +1,14 @@
 import httpError from "../helpers/httpError.js";
 
-const validateBody = (schema, errorType) => {
+const validateBody = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
       let errorMessage;
-      switch (errorType) {
-        case "post":
-          if (error.details[0].type === "any.required") {
-            errorMessage = `missing required ${error.details[0].context.key} field`;
-          } else {
-            errorMessage = error.details[0].message;
-          }
-          break;
-        case "put":
-          if (error.details[0].context.key) {
-            errorMessage = "missing fields";
-          } else {
-            errorMessage = error.details[0].message;
-          }
-          break;
+      if (!req.body || Object.keys(req.body).length === 0) {
+        errorMessage = "missing fields";
+      } else {
+        errorMessage = error.details[0].message;
       }
       next(httpError(400, errorMessage));
     } else {
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,29 @@
 import Joi from "joi";
 
 const addSchema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
-    email: Joi.string().email().required(),
-    phone: Joi.string().min(10).max(15).required(),
-  });
+  name: Joi.string()
+    .min(2)
+    .max(30)
+    .required()
+    .messages({
+      "string.min": "The minimum length is 2 characters",
+      "string.max": "The maximum is 30 characters",
+      "any.required": "missing required name field",
+    }),
+  email: Joi.string()
+    .email()
+    .required()
+    .messages({
+      "string.email": "Invalid email format",
+      "any.required": "missing required email field",
+    }),
+  phone: Joi.string()
+    .pattern(/^[+]\d{1,4}-\d{1,3}-\d{1,4}-\d{1,4}-\d{1,9}$/)
+    .required()
+    .messages({
+      "string.pattern.base": "The correct number format should be: +xx-xxx-xxx-xx-xx",
+      "any.required": "missing required phone field",
+    }),
+});
 
-  export default addSchema;
+export default addSchema;
