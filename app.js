e4e893ac0c754a1707fb086f9d1c374cc02677e6
Oleksandr <live.gp@gmail.com>
2023-09-29 22:45:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express, { json } from 'express'
 import logger from 'morgan'
 import cors from 'cors'
 import contactsRouter from './routes/api/contacts.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
