8a69ca766a68ae7ac1b6fc0364b1380627831b84
Oleksandr <live.gp@gmail.com>
2023-09-29 21:35:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-import app from './app.js';
+import mongoose from "mongoose";
+import app from "./app.js";
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Server running. Use our API on port: ${port}`);
-});
+const DB_HOST =
+  "mongodb+srv://livegp:nVXOubphr3jQ2O1z@cluster0.yd5j5qv.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    const port = process.env.PORT || 3000;
+    app.listen(port);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message)
+    process.exit(1);
+  })
