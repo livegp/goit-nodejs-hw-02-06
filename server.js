d4d213dd375298543adf7d5219903cf0d92e317d
Oleksandr <live.gp@gmail.com>
2023-09-30 13:38:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const {DB_HOST, PORT = 3000} = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
+    console.log("Database connection successful");
+    app.listen(PORT, () => console.log(`Server running: http://localhost:${PORT}`));
   })
   .catch((error) => {
     console.log(error.message)
