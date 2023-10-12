6b60b51db3d53867f128fad2d7bd0952b1fbb775
Oleksandr <live.gp@gmail.com>
2023-10-12 11:09:38 +0300

    fix url

--- controllers/auth.js
+++ controllers/auth.js
@@ -87,15 +87,15 @@ const updateSubscription = async (req, res) => {
 };
 
 const updateAvatar = async (req, res, next) => {;
-  const avatarsDir = path.resolve("public", "avatars");
+  const avatarsDir = path.join("public", "avatars");
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
-  const resultUpload = path.resolve(avatarsDir, filename);
+  const resultUpload = path.join(avatarsDir, filename);
   const image = await Jimp.read(tempUpload);
   image.resize(250, 250).write(tempUpload);
   await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.resolve("avatars", filename);
+  const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({ avatarURL });
 };
